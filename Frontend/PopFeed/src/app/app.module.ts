40ce46e0f2994d9fcfe5ab7fa323cc12b6349d09
ParkerKalthoff <parkerlkalthoff@gmail.com>
2023-05-31 00:04:22 -0500

    pee pod

--- Frontend/PopFeed/src/app/app.module.ts
+++ Frontend/PopFeed/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { BookmarkedComponent } from './bookmarked/bookmarked.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent,
+    NotificationsComponent,
+    BookmarkedComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
